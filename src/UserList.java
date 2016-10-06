777d4899a70f876363956d3de9ec8051087fa786
Colten Leidner <SIU851463675@MDV0715DVHRJ52.ad.siu.edu>
2016-10-06 11:34:50 -0500

    Created a method to remove a user form linked list

--- src/UserList.java
+++ src/UserList.java
@@ -5,16 +5,26 @@ public class UserList {
 	
 	public UserList(){
 		TotalUsers=0;
+		first=null;
 	}
 	
-	public void AddUser(UserProfile user){
-		if(TotalUsers==0){
-			first=user;
-			user.NextUser=null;
-		}
-		else{
+	private void AddUser(UserProfile user){
 		user.NextUser=first;
 		first=user;
+	}
+	
+	private void RemoveUser(UserProfile user){
+		if (first==user){
+			UserProfile temp = first.NextUser;
+			first.NextUser=null;
+			first=temp;
+		}
+		else{
+		UserProfile currentuser = first;
+			while(currentuser.NextUser!=user){
+			currentuser=currentuser.NextUser;
+			}
+		currentuser.NextUser=currentuser.NextUser.NextUser;
 		}
 	}
 }
